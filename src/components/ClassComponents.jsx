cd8274c5f23d3a28144f1e7b97f66018be03b070
Adwaith360 <adwaitham360@gmail.com>
2023-06-08 10:49:50 +0530

    completed first part of the project

---
+++ src/components/ClassComponents.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+class ClassComponent extends react.Component{
+    render(){
+    return(
+        <h1> Hello World</h1>
+
+    );
+}
+}
+
+export default ClassComponent;
---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react" ;
+
+function FunctionalComponent(){
+return(
+    <h1>This is a FunctionalComponent</h1>
+)
+}
+
+export default FunctionalComponent;
